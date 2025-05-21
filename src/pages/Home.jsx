28b4632f9c901ccad8bc0fa25dbdc1e91d0cc49d
NipulPanit <pnipul7678@gmail.com>
2025-05-22 00:30:00 +0530

    seo done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,21 +4,22 @@ function Home() {
   return (
     <section
       className="w-full h-full bg-cover bg-center flex  justify-center items-center px-4"
-      style={{ backgroundImage: "url(bg.jpg)" }}
+      style={{ backgroundImage: "url(turtlabs.jpg)" }}
     >
       <div className="sm:container sm:mx-auto sm:max-w-3/6 flex sm:flex-row flex-col justify-between items-start backdrop-blur-xs bg-black/30 border border-white/5 rounded-lg px-5 py-8 gap-y-5">
         <div className="flex-2 self-stretch">
-          <p className="sm:text-lg text-white mb-1 text-md">
-            FullStack Developer
-          </p>
-          <h1 className="lg:text-8xl md:text-7xl sm:text-6xl text-5xl text-white">
-            Nipul Pandit
+          <h2 className="sm:text-lg text-white mb-1 text-md">
+            We Dive Deep Into What You Need
+          </h2>
+          <h1 className="lg:text-8xl md:text-7xl sm:text-6xl text-6xl text-white">
+            The Turtlabs
           </h1>
-          <p className="sm:text-md text-white/80 mt-12 text-sm">
-            I specialize in building responsive, high-performance web
-            applications using modern technologies like React, Node.js, and
-            MongoDB. I focus on clean code, strong UI/UX, and scalable backend
-            solutions.
+          <p className="sm:text-md text-white/80 mt-10 text-sm italic">
+            We specialize in crafting responsive, high-performance web and
+            mobile applications using modern technologies including React,
+            Next.js, Node.js, MongoDB, and React Native. Our focus is on clean,
+            maintainable code, exceptional UI/UX design, and scalable, secure
+            backend solutions.
           </p>
         </div>
         <div className="flex-1 flex flex-row sm:flex-col sm:justify-between justify-start items-center gap-x-3 self-stretch ">
