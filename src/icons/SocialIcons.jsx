29565914bca5f98e2215fe079004efe372b4c827
NipulPanit <pnipul7678@gmail.com>
2025-05-23 21:53:13 +0530

    final design

--- src/icons/SocialIcons.jsx
+++ src/icons/SocialIcons.jsx
@@ -2,9 +2,12 @@ import React from "react";
 
 function SocialIcons({ link, icons, linkType }) {
   return (
-    <a href={`${linkType ? linkType + link : link}`} aria-label="social-link">
+    <a
+      href={`${linkType ? linkType + link : link}`}
+      aria-label="social-link"
+    >
       <i
-        className={`${icons} text-black sm:w-15 sm:h-15 w-10 h-10 sm:text-4xl text-2xl rounded-full flex justify-center items-center bg-white`}
+        className={`${icons} text-black sm:w-15 sm:h-15 w-10 h-10 sm:text-4xl text-2xl rounded-full flex justify-center items-center bg-white hover:scale-75 transition-all ease-in duration-300`}
       ></i>
     </a>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,23 +13,24 @@ function Home() {
         className="w-full h-full bg-cover bg-center flex  justify-center items-center px-4"
         style={{ backgroundImage: "url(turtlabs.webp)" }}
       >
-        <div className="sm:container sm:mx-auto sm:max-w-3/6 flex sm:flex-row flex-col justify-between items-start backdrop-blur-xs bg-black/30 border border-white/5 rounded-lg px-5 py-8 gap-y-5">
+        <div className="sm:container sm:mx-auto sm:max-w-3/6 flex sm:flex-row flex-col justify-between sm:items-center items:start backdrop-blur-xl border border-white/2 rounded-lg px-5 py-8 gap-y-5 w-full relative">
           <div className="flex-2 self-stretch">
-            <h2 className="sm:text-lg text-white mb-1 text-md">
-              We Dive Deep Into What You Need
+            <h2 className="sm:text-lg text-white mb-10 text-md font-extralight">
+              We Dive Deep Into What You Need...
             </h2>
             <h1 className="lg:text-8xl md:text-7xl sm:text-6xl text-6xl text-white">
-              The Turtlabs
+              The <br />
+              <span>Turtlabs</span>
             </h1>
-            <p className="sm:text-md text-white/80 mt-10 text-sm italic">
-              We specialize in crafting responsive, high-performance web and
-              mobile applications using modern technologies including React,
-              Next.js, Node.js, MongoDB, and React Native. Our focus is on
-              clean, maintainable code, exceptional UI/UX design, and scalable,
-              secure backend solutions.
+            <p className="sm:text-md text-white mt-10 font-extralight">
+              We build responsive web and mobile applications with seamless user
+              experiences and scalable backend systems. Our solutions combine
+              modern frontend interfaces with intuitive UI/UX and secure,
+              high-performance backend architectures tailored for long-term
+              growth.
             </p>
           </div>
-          <div className="flex-1 flex flex-row sm:flex-col sm:justify-between justify-start items-center gap-x-3 self-stretch ">
+          <div className="flex-1 flex flex-row sm:flex-col sm:justify-between justify-start items-center gap-x-3 sm:gap-y-14  ">
             <SocialIcons
               link="https://wa.me/9205470224"
               icons="ri-whatsapp-line"
@@ -50,6 +51,8 @@ function Home() {
             />
           </div>
         </div>
+
+        <p className="fixed bottom-5 text-white text-sm">&copy; {new Date().getFullYear()} Turtlabs, All Rights Reserved</p>
       </section>
     </>
   );
